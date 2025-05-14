956da9ba77f126523fe0fb810f793590f53ed5be
sivabharath23 <sivabharath23@gmail.com>
2025-05-14 15:53:41 +0530

    calcualted result

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 👈 This makes the dev server listen on all local IPs
-    port: 5173, // optional: default port
+    host: true,
+    port: 5173,
+    base: '/',
+
   },
 });
+
